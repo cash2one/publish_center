355fd25ad4c7ff83c93e1d441ac4c87882093f90
lrqrun <lrqrun@gmail.com>
2016-07-26 18:49:53 +0800

    fix bug

---
+++ run_server.py
@@ -0,0 +1,61 @@
+# encoding: utf-8
+
+"""
+@version: 
+@author: liriqiang
+@file: run_server.py
+@time: 16-7-26 下午4:02
+"""
+import os
+
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
+import tornado.gen
+import tornado.httpclient
+from tornado.websocket import WebSocketClosedError
+from tornado.options import define, options
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'publish_center.settings'
+from publish_center.settings import IP, PORT
+
+define("port", default=PORT, help="run on the given port", type=int)
+define("host", default=IP, help="run port on given host", type=str)
+
+
+def main():
+    from django.core.wsgi import get_wsgi_application
+    import tornado.wsgi
+    wsgi_app = get_wsgi_application()
+    container = tornado.wsgi.WSGIContainer(wsgi_app)
+    setting = {
+        'cookie_secret': 'DFksdfsasdfkasdfFKwlwfsdfsa1204mx',
+        'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+        'static_path': os.path.join(os.path.dirname(__file__), 'static'),
+        'debug': False,
+    }
+    tornado_app = tornado.web.Application(
+        [
+            (r"/static/(.*)", tornado.web.StaticFileHandler,
+             dict(path=os.path.join(os.path.dirname(__file__), "static"))),
+            ('.*', tornado.web.FallbackHandler, dict(fallback=container)),
+        ], **setting)
+
+    server = tornado.httpserver.HTTPServer(tornado_app)
+    server.listen(options.port)
+
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    # tornado.options.parse_command_line()
+    # app = Application()
+    # server = tornado.httpserver.HTTPServer(app)
+    # server.bind(options.port, options.host)
+    # #server.listen(options.port)
+    # server.start(num_processes=5)
+    # tornado.ioloop.IOLoop.instance().start()
+    print "Run server on %s:%s" % (options.host, options.port)
+    main()
---
+++ service.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# publish_center        Startup script for the publish_center Server
+
+publish_center_dir=
+
+base_dir=$(dirname $0)
+publish_center_dir=${publish_center_dir:-$base_dir}
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+if [ -f ${publish_center_dir}/commands/functions ];then
+    . ${publish_center_dir}/commands/functions
+elif [ -f /etc/init.d/functions ];then
+    . /etc/init.d/functions
+else
+    echo "No functions script found in [./functions, ./install/functions, /etc/init.d/functions]"
+    exit 1
+fi
+
+PROC_NAME="publish_center"
+lockfile=/var/lock/subsys/${PROC_NAME}
+
+start() {
+        ops_start=$"Starting ${PROC_NAME} service:"
+        if [ -f $lockfile ];then
+             echo -n "publish_center is running..."
+             success "$ops_start"
+             echo
+        else
+            daemon $publish_center_dir/venv/bin/python $publish_center_dir/manage.py crontab add &>> /var/log/publish_center.log 2>&1
+            daemon $publish_center_dir/venv/bin/python $publish_center_dir/run_server.py &> /dev/null 2>&1 &
+            sleep 1
+            echo -n "$ops_start"
+            ps axu | grep 'run_server' | grep -v 'grep' &> /dev/null
+            if [ $? == '0' ];then
+                success "$ops_start"
+                if [ ! -e $lockfile ]; then
+                    lockfile_dir=`dirname $lockfile`
+                    mkdir -pv $lockfile_dir
+                fi
+                touch "$lockfile"
+                echo
+            else
+                failure "$ops_start"
+                echo
+            fi
+        fi
+}
+
+
+stop() {
+    echo -n $"Stopping ${PROC_NAME} service:"
+    daemon $publish_center_dir/venv/bin/python $publish_center_dir/manage.py crontab remove &>> /var/log/publish_center.log 2>&1
+    ps aux | grep -E 'run_server.py' | grep -v grep | awk '{print $2}' | xargs kill -9 &> /dev/null
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        echo_success
+        echo
+        rm -f "$lockfile"
+    else
+        echo_failure
+        echo
+        rm -f "$lockfile"
+    fi
+
+}
+
+status(){
+    ps axu | grep 'run_server' | grep -v 'grep' &> /dev/null
+    if [ $? == '0' ];then
+        echo -n "publish_center is running..."
+        success
+        touch "$lockfile"
+        echo
+    else
+        echo -n "publish_center is not running."
+        failure
+        echo
+    fi
+}
+
+
+
+restart(){
+    stop
+    start
+}
+
+# See how we were called.
+case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
+
+  restart)
+        restart
+        ;;
+
+  status)
+        status
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart|status}"
+        exit 2
+esac
